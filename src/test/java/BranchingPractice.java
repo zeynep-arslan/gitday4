be3aea3d40e96988118baa8bcae5faa9ea24b048
zeynep-arslan <zeyzey9444@gmail.com>
2022-06-26 23:26:15 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,5 +2,10 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        // created new project
+        //enable version control
+        //added .gitignore with .idea , target folder and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
     }
 }
